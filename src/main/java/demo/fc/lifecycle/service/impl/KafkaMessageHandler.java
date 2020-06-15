c7cd1e15b421689576cc1dc4399f190ff339a60c
方超 <chao.fang@hand-china.com>
2020-06-15 10:10:07 +0800

    update:set kafka listener group id

--- src/main/java/demo/fc/lifecycle/service/impl/KafkaMessageHandler.java
+++ src/main/java/demo/fc/lifecycle/service/impl/KafkaMessageHandler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaMessageHandler {
 
-    @KafkaListener(topics = {"test-topic"})
+    @KafkaListener(topics = {"test-topic"}, groupId = "kafka-test")
     public void handleKafkaMessage(ConsumerRecord record) {
         System.out.println(record.value().toString());
 
